aad3aefede2ac38f0ebb1b08959921a9592d101a
zveand <127590811+zveand@users.noreply.github.com>
2023-03-14 18:30:16 +0200

    Add files via upload

---
+++ askisi1/function.h
@@ -0,0 +1,5 @@
+long long max(int a, int b){
+    int temp_var;
+  temp_var=a*b;
+    return temp_var;
+}
---
+++ askisi1/tutorial.c
@@ -0,0 +1,19 @@
+#include "function.h"
+#include <stdio.h>
+int a, b;
+int res;
+
+int main(){
+	
+	a=1221;
+    b=666;
+    res=max(a,b);
+    
+/*	char *filename="text.txt";
+	FILE *fp = fopen(filename,"w");
+	fprintf(fp,"#%llu \n",res);
+	fclose(fp);
+*/
+   
+    return res;
+}
